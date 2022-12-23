ea0139e4cdb2e7d604ed60b26ba7e96c7fa7547a
scx1332 <sieciech.czajka@golem.network>
2022-12-23 14:38:12 +0100

    Working on frontend

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -3,14 +3,24 @@ import './Dashboard.css';
 import TxBox from "./TxBox";
 
 const BACKEND_URL = "http://localhost:8080"
+const MAX_VISIBLE_TXS = 10;
 
 const Dashboard = () => {
-    const [nextTxsReversed, setNextTxsReversed] = React.useState(null);
+    const [nextRefresh, setNextRefresh] = React.useState(0);
+    const [nextTxsReversed, _setNextTxsReversed] = React.useState(null);
+    function setNextTxsReversed(txs: any) {
+        if (txs != null) {
+            if (txs.length >= MAX_VISIBLE_TXS) {
+                txs[MAX_VISIBLE_TXS - 1].opacity = 0;
+                txs[MAX_VISIBLE_TXS - 1].maxHeight = 0;
+            }
+            _setNextTxsReversed(txs.slice(0, MAX_VISIBLE_TXS));
+        } else {
+            _setNextTxsReversed(txs);
+        }
+    }
 
     const [txCount, setTxCount] = React.useState(null)
-    const [lastTxs, setLastTxs] = React.useState(null)
-    const [nextTxs, setNextTxs] = React.useState(null)
-    const [currentTxs, setCurrentTxs] = React.useState(null)
 
 
 
@@ -20,91 +30,104 @@ const Dashboard = () => {
         const response_json = await response.json();
         setTxCount(response_json);
     }
-    const loadTxsNext = async () => {
-        const response = await fetch(`http://127.0.0.1:8080/transactions/next/2`);
+    const loadTxsFeed = async () => {
+        const response = await fetch(`http://127.0.0.1:8080/transactions/feed/5/2`);
         const response_json = await response.json();
-        let reversed = response_json.txs.slice().reverse();
-        setNextTxsReversed(reversed);
-    }
-    const loadTxsCurrent = async () => {
-        const response = await fetch(`http://127.0.0.1:8080/transactions/current`);
-        const response_json = await response.json();
-        setCurrentTxs(response_json);
-    }
-    const loadTxsLast = async () => {
-        const response = await fetch(`http://127.0.0.1:8080/transactions/last/5`);
-        const response_json = await response.json();
-        setLastTxs(response_json);
-    }
-    const loadNextClick = async () => {
-        const response = await fetch(`http://127.0.0.1:8080/transactions/next/2`);
-        const response_json = await response.json();
-        let nReversed = nextTxsReversed.slice();
-        let txToAdd = [];
-        for (const nextTx of response_json.txs) {
-            let found = false;
-            for (const currentTx of nextTxsReversed) {
-                if (currentTx.id == nextTx.id) {
-                    found = true;
-                    break;
+        let reversed = response_json.txs.slice().reverse().map((tx: any) => {return {"key": tx.tx_id, "data": tx}});
+
+        if (nextTxsReversed == null) {
+            setNextTxsReversed(reversed);
+            return;
+        } else {
+            let nReversed = nextTxsReversed.slice();
+            let txToAdd = [];
+            for (let entry of reversed) {
+                let nextTx = entry.data;
+                let foundIdx = -1;
+                for (let idx = 0; idx < nextTxsReversed.length; idx++) {
+                    if (nextTxsReversed[idx].data.id == nextTx.id) {
+                        foundIdx = idx;
+                        break;
+                    }
+                }
+                if (foundIdx == -1) {
+                    console.log(`next id: ${nextTx.id}`);
+                    entry.opacity = 0.0;
+                    entry.maxHeight = 0;
+                    txToAdd.push(entry);
+                } else {
+                    nReversed[foundIdx].data = nextTx;
                 }
             }
-            if (!found) {
-                console.log(`next id: ${nextTx.id}`);
-                nextTx.opacity = 0.0;
-                nextTx.maxHeight = 10;
-                txToAdd.push(nextTx);
-                setNextTxsReversed([nextTx, ...nReversed]);
+            if (txToAdd.length > 0) {
+                let newReversed = [...txToAdd, ...nReversed];
+                setNextTxsReversed(newReversed);
+                await new Promise(r => setTimeout(r, 100));
+                for (let tx of newReversed) {
+                    tx.opacity = 1.0;
+                    tx.maxHeight = 500;
+                }
+                setNextTxsReversed(newReversed.slice());
             }
         }
-        if (txToAdd.length > 0) {
-            setNextTxsReversed([...txToAdd, ...nReversed]);
-            await new Promise(r => setTimeout(r, 50));
-            for (const tx of txToAdd) {
-                tx.opacity = 1.0;
-                tx.maxHeight = 500;
-            }
-            setNextTxsReversed([...txToAdd, ...nReversed]);
-        }
+    }
+    const loadNextClick = async () => {
+        loadTxsFeed().then(() => {
+
+        });
     }
 
     const loadDashboard = async () => {
-        const fut2 = loadTxsNext();
-        const fut3 = loadTxsCurrent();
-        const fut4 = loadTxsLast();
-        const fut5 = loadTxCount();
-        await Promise.all([fut2, fut3, fut4, fut5]);
+        const futFeed = loadTxsFeed();
+        const futCount = loadTxCount();
+        await Promise.all([futFeed, futCount]);
+        await new Promise(r => setTimeout(r, 5000));
     }
 
 
     React.useEffect(() => {
         console.log("useEffect");
         loadDashboard().then(() => {
+            setNextRefresh(nextRefresh + 1);
         });
     }, [])
 
-
+    React.useEffect(() => {
+        if (nextRefresh > 0) {
+            loadDashboard().then(() => {
+                setNextRefresh(nextRefresh + 1);
+            });
+        }
+    }, [nextRefresh])
 
     function row(tx: any) {
-        let opacity = tx.opacity ?? 0.5;
-        let maxHeight = tx.maxHeight ?? 500;
+        let opacity = tx.opacity ?? 1.0;
+        let maxHeight = tx.maxHeight ?? 200;
 
-        return (<div key={tx.id} style={{opacity: opacity, maxHeight: maxHeight}} className={"tx-wrapper"}>
-            <TxBox tx_id={tx.id} />
+        return (<div key={tx.data.id} style={{opacity: opacity, maxHeight: maxHeight}} className={"tx-wrapper"}>
+            <TxBox tx_id={tx.data.id} tx={tx.data} />
             <hr/>
         </div>)
+    }
+
+    let displayTxCount = 0;
+    if (nextTxsReversed) {
+        for (let tx of nextTxsReversed) {
+            if (tx.opacity === undefined || tx.opacity > 0) {
+                displayTxCount++;
+            }
+        }
     }
 
     return (
         <div>
             <h1>Dashboard</h1>
+            <div>Displaying {displayTxCount} transactions out of {txCount?.txDone} finished transaction and {txCount?.txQueued} queued</div>
             <button onClick={loadNextClick}>Load new</button>
 
-            {(lastTxs != null && currentTxs != null && nextTxsReversed != null) ? (
+            {(nextTxsReversed != null) ? (
                 <div className={"tx-33"}>
                     {nextTxsReversed.map(row)}
-                    {currentTxs.txs.map(row)}
-                    {lastTxs.txs.map(row)}
                 </div>
                 ) : (
                 <div>Loading...</div>
--- src/TxBox.tsx
+++ src/TxBox.tsx
@@ -2,38 +2,44 @@ import React from "react";
 import "./TxBox.css"
 import DateBox from "./DateBox";
 import TransfersBox from "./TransfersBox";
+import {Circles} from 'react-loading-icons'
+import {BiTime, BiError} from "react-icons/bi";
+import {GiConfirmed} from "react-icons/gi";
+
 
 interface TransactionProps {
     tx_id: number | null,
+    tx: any
 }
 
 const TxBox = (props: TransactionProps) => {
-    let [tx, setTx] = React.useState(null);
+    //let [tx, setTx] = React.useState(null);
 
-    const loadTxDetails = async () => {
+    /*const loadTxDetails = async () => {
         if (props.tx_id) {
             const response = await fetch(`http://127.0.0.1:8080/tx/${props.tx_id}`);
             const response_json = await response.json();
 
             setTx(response_json.tx);
         }
-    }
+    }*/
 
-    const loadData = async () => {
+    /*const loadData = async () => {
         for (let loopNo = 0; ; loopNo++) {
             loadTxDetails().then(() => {
             });
             await new Promise(r => setTimeout(r, 2000));
         }
-    }
+    }*/
 
-    React.useEffect(() => {
+    /*React.useEffect(() => {
         loadData().then(() => {
         });
-    }, [props.tx_id])
+    }, [props.tx_id])*/
 
+    let tx = props.tx;
     if (tx == null) {
-        return (<div></div>)
+        return (<div className={"tx-container-wrapper"}></div>)
     }
 
     let feePaid: number | null = null;
@@ -42,8 +48,36 @@ const TxBox = (props: TransactionProps) => {
         let feePaidWei = BigInt(tx.feePaid) / BigInt(1000000000);
         feePaid = Number(feePaidWei) / 1000000000;
     }
+
+    function SkipTx() {
+        fetch(`http://127.0.0.1:8080/tx/skip/${props.tx_id}`, {method: 'POST'}).then((resp) => {
+            console.log(resp);
+        });
+    }
+
+    function GetIcon() {
+        if (tx.confirmDate != null && tx.error == null && tx.engineError == null) {
+            return <GiConfirmed className={"tx-ok-icon"}/>
+        } else if (tx.engineError != null) {
+            return <BiError className={"tx-error-icon"}/>
+        } else if (tx.error != null) {
+            return <BiError className={"tx-error-icon"}/>
+        } else if (tx.engineMessage) {
+            return <Circles className={"tx-loading"} fill="#000000"/>
+        } else {
+            return <BiTime className={"tx-wait-icon"}/>
+        }
+    }
+    function GetActions() {
+        if (tx.engineMessage) {
+            return <button onClick={SkipTx}>Skip transaction</button>
+        } else {
+            return <></>
+        }
+    }
     return (
         <div className={"tx-container-wrapper"}>
+
             <div className={"tx-container"}>
                 <div className={"tx-id"}>db id: {tx.id}</div>
                 <div className={"chain-id"}>chain id: {tx.chainId}</div>
@@ -82,8 +116,13 @@ const TxBox = (props: TransactionProps) => {
                 <div className={"tx-fee tx-fee-unknown"}>
                     fee paid: N/A
                 </div>}
-                <div className={"tx-error"}>
-                    {"test error"}
+
+                <div className={"tx-processing"}>
+                    {GetIcon()}
+                </div>
+                <div className={"tx-message"}>
+                    {GetActions()}
+                    {tx.engineMessage} <span className={"tx-message-error"}>{tx.engineError} {tx.error}</span>
                 </div>
             </div>
             <div className={"tx-transfers"}>
