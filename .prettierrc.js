27b8f452121eccb62bc9fb45e4515df6c700d48e
scx1332 <sieciech.czajka@golem.network>
2023-01-07 22:01:49 +0100

    Prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,28 @@
 module.exports = {
-    "trailingComma": "all",
-    "tabWidth": 2,
-    "semi": true,
-    "singleQuote": true,
-    "printWidth": 120,
-    "bracketSpacing": true
-}
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  printWidth: 120,
+  bracketSpacing: true,
+  overrides: [
+    {
+      files: ["*.json"],
+      options: {
+        tabWidth: 2,
+      },
+    },
+    {
+      files: ["*.tsx"],
+      options: {
+        tabWidth: 2,
+      },
+    },
+    {
+      files: ["*.html"],
+      options: {
+        tabWidth: 2,
+      },
+    },
+  ],
+};
